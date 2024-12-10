f4496e182c896b943bb52a38bda18217fc792bdc
asd9826 <anildeo2601@gmail.com>
2024-12-10 23:53:15 +1100

    Create circleshape.py

--- main.py
+++ main.py
@@ -12,8 +12,11 @@ def main():
         for event in pygame.event.get():
              if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen,(0,0,0))
-        dt = (clock.tick(60))/1000
+        
+        screen.fill("black")
+        pygame.display.flip()
+        
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
