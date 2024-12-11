8207475308f63d374fc2df7dfeca4bd4eb556008
asd9826 <anildeo2601@gmail.com>
2024-12-11 14:21:27 +1100

    Add Shooting

--- circleshape.py
+++ circleshape.py
@@ -21,6 +21,6 @@ class CircleShape(pygame.sprite.Sprite):
         # sub-classes must override
         pass
     def collision(self, other):
-        return pygame.math.Vector2.distance_to(self.position, other.position) <= self.radius + other.radius   
+        return self.position.distance_to(other.position) <= self.radius + other.radius   
 
 
